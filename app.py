b5fee141fe273ac6fe1228b11066ab902e0f2459
Ahmat293 <goukouni500@>
2026-03-05 11:43:04 +0100

    use tfidf model

--- app.py
+++ app.py
@@ -1,32 +1,24 @@
 import streamlit as st
-import torch
 import pickle
 from huggingface_hub import hf_hub_download
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-st.title("Analyse de sentiment - CamemBERT")
+st.title("Analyse de sentiment - Ynov")
 
 @st.cache_resource
 def load_model():
-    tokenizer = AutoTokenizer.from_pretrained("Ahmat293/camembert-sentiment-ynov", subfolder="tokenizer")
-    model = AutoModelForSequenceClassification.from_pretrained("Ahmat293/camembert-sentiment-ynov", subfolder="model")
-    le_path = hf_hub_download(repo_id="Ahmat293/camembert-sentiment-ynov", filename="label_encoder.pkl")
-    with open(le_path, "rb") as f:
-        le = pickle.load(f)
-    model.eval()
-    return tokenizer, model, le
+    model_path = hf_hub_download(repo_id="Ahmat293/camembert-sentiment-ynov", filename="sentiment_model.pkl")
+    with open(model_path, "rb") as f:
+        pipeline = pickle.load(f)
+    return pipeline
 
-tokenizer, model, le = load_model()
+pipeline = load_model()
 
 comment = st.text_area("Entrez un commentaire :")
 
 if st.button("Analyser"):
     if comment:
-        inputs = tokenizer(comment, return_tensors="pt", truncation=True, max_length=128)
-        with torch.no_grad():
-            pred = model(**inputs).logits.argmax(dim=1).item()
-        sentiment = le.inverse_transform([pred])[0]
-        print(sentiment)
+        sentiment = pipeline.predict([comment])[0]
+        
         if sentiment == "positif":
             st.success(f"Sentiment : {sentiment} 😊")
         elif sentiment == "negatif":
